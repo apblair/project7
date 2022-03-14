cc3d4a80a812281b72422f31fc188bd778eab870
Andrew Blair <andrew.blair@ucsf.edu>
2022-03-13 22:46:37 -0700

    v0.1.0 NeuralNetwork.backprop

--- nn/nn.py
+++ nn/nn.py
@@ -162,9 +162,9 @@ class NeuralNetwork:
             # Run forward propagation
             layer_idx = idx + 1
             A_curr, Z_curr = self._single_forward(self._param_dict['W'+str(layer_idx)], # W_curr
-                                self._param_dict['b'+str(layer_idx)], # b_curr
-                                A_prev, # A_prev
-                                layer['activation']) # activation
+                                                self._param_dict['b'+str(layer_idx)], # b_curr
+                                                A_prev, # A_prev
+                                                layer['activation']) # activation
             A_prev = A_curr # set layer activation matrix
             # Store Z and A matrices in cache dictionary for use in backprop
             cache["A" + str(layer_idx)] = A_curr 
@@ -226,7 +226,19 @@ class NeuralNetwork:
                 Dictionary containing the gradient information from this pass of backprop.
         """
         loss_function = self._select_function(function_type='loss', loss=self._loss_func)
+        dA_curr = loss_function(y, y_hat)
         grad_dict = {}
+        for idx, layer in enumerate(self.arch)[::-1]:
+            layer_idx = idx + 1
+            dA_prev, dW_curr, db_curr = self._single_backprop(self._param_dict['W'+str(layer_idx)], # W_curr
+                                                            self._param_dict['b'+str(layer_idx)], # b_curr
+                                                            cache['Z'+ str(layer_idx)], # Z_curr
+                                                            cache['A'+ str(layer_idx)], # A_prev
+                                                            dA_curr, # dA_curr
+                                                            layer['activation']) # activation_curr
+            print(dA_prev, dW_curr, db_curr)
+        # return grad_dict
+
 
     def _update_params(self, grad_dict: Dict[str, ArrayLike]):
         """
